8b3f425466c1b4bcf5605e8be91f6dd4e130efe2
yyy <1232423@xx.com>
2024-10-16 17:48:52 +0800

    修改input 大小

--- src/components/StockConfig.js
+++ src/components/StockConfig.js
@@ -15,39 +15,63 @@ const StockConfig = () => {
       <div className="col-12 col-md-4">
         <div className="form-group">
           <label className="d-block mb-2">首日量比大于</label>
-          <input type="number" className="form-control text-left" value={firstDayVolRatio} onChange={(e) => setFirstDayVolRatio(e.target.value)} />
+          <div className="row">
+            <div className="col-sm-3">
+              <input type="number" className="form-control text-left" value={firstDayVolRatio} onChange={(e) => setFirstDayVolRatio(e.target.value)} />
+            </div>
+          </div>
         </div>
         <div className="form-group">
           <label className="d-block mb-2">第二个阳线收盘价达到X交易日新高</label>
-          <input type="number" className="form-control text-left" value={secondCandleNewHighDays} onChange={(e) => setSecondCandleNewHighDays(e.target.value)} />
+          <div className="row">
+            <div className="col-sm-3">
+              <input type="number" className="form-control text-left" value={secondCandleNewHighDays} onChange={(e) => setSecondCandleNewHighDays(e.target.value)} />
+            </div>
+          </div>
         </div>
       </div>
       <div className="col-12 col-md-4">
         <div className="form-group">
           <label className="d-block mb-2">第一个阳线前一天的自由流通市值范围 (单位：亿)</label>
           <div className="input-group">
-            <input type="number" className="form-control text-left" value={freeFloatValueRangeMin} onChange={(e) => setFreeFloatValueRangeMin(e.target.value)} />
+            <div className="col-sm-3">
+              <input type="number" className="form-control text-left" value={freeFloatValueRangeMin} onChange={(e) => setFreeFloatValueRangeMin(e.target.value)} />
+            </div>
             <div className="input-group-text">-</div>
-            <input type="number" className="form-control text-left" value={freeFloatValueRangeMax} onChange={(e) => setFreeFloatValueRangeMax(e.target.value)} />
+            <div className="col-sm-3">
+              <input type="number" className="form-control text-left" value={freeFloatValueRangeMax} onChange={(e) => setFreeFloatValueRangeMax(e.target.value)} />
+            </div>
           </div>
         </div>
         <div className="form-group">
           <label className="d-block mb-2">第一个阳线前一天的流通市值范围 (单位：亿)</label>
           <div className="input-group">
-            <input type="number" className="form-control text-left" value={circulationValueRangeMin} onChange={(e) => setCirculationValueRangeMin(e.target.value)} />
+            <div className="col-sm-3">
+              <input type="number" className="form-control text-left" value={circulationValueRangeMin} onChange={(e) => setCirculationValueRangeMin(e.target.value)} />
+            </div>
             <div className="input-group-text">-</div>
-            <input type="number" className="form-control text-left" value={circulationValueRangeMax} onChange={(e) => setCirculationValueRangeMax(e.target.value)} />
+            <div className="col-sm-3">
+              <input type="number" className="form-control text-left" value={circulationValueRangeMax} onChange={(e) => setCirculationValueRangeMax(e.target.value)} />
+            </div>
           </div>
         </div>
       </div>
       <div className="col-12 col-md-4">
         <div className="form-group">
           <label className="d-block mb-2">10日线均线放大系数</label>
-          <input type="number" className="form-control text-left" value={ma10Ratio} onChange={(e) => setMa10Ratio(e.target.value)} />
+          <div className="row">
+            <div className="col-sm-3">
+              <input type="number" className="form-control text-left" value={ma10Ratio} onChange={(e) => setMa10Ratio(e.target.value)} />
+            </div>
+          </div>
         </div>
         <div className="form-group">
           <label className="d-block mb-2">回踩10日均线后第一个阳线的小于交易日</label>
-          <input type="number" className="form-control text-left" value={daysToMa10} onChange={(e) => setDaysToMa10(e.target.value)} />
+          <div className="row">
+            <div className="col-sm-3">
+              <input type="number" className="form-control text-left" value={daysToMa10} onChange={(e) => setDaysToMa10(e.target.value)} />
+            </div>
+          </div>
         </div>
       </div>
     </div>
