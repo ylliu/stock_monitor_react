fd32185cabb80b443b353a1ca608439807e1e9e7
ylliu <ylliu@qq.com>
2024-11-09 00:51:37 +0800

    add  fenshi

---
+++ stock-monitor/src/components/hqchart.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const HQChart = () => {
+  return (
+    <div style={{ width: '100vh', height: '100vh' }}>
+      <iframe
+        src="https://gu.qq.com/sz300377/gp"
+        title="HQChart"
+        width="100%"
+        height="100%"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
+};
+
+export default HQChart;
